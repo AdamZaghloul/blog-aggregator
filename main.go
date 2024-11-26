f1c663889adcc24e91198243c35a200f688746b2
AdamZaghloul <adam.zaghloul95@gmail.com>
2024-11-25 20:58:23 -0500

    Added login command.

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"blog-aggregator/internal/config"
+	"errors"
 	"fmt"
+	"os"
 )
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
 
 func main() {
 	cfg, err := config.Read()
@@ -11,13 +26,65 @@ func main() {
 		fmt.Println(err)
 	}
 
-	cfg.SetUser("adam")
+	ste := state{}
+	ste.config = &cfg
 
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println(err)
+	cmds := commands{
+		cmds: map[string]func(*state, command) error{
+			"login": handlerLogin,
+		},
 	}
 
-	fmt.Println(cfg.CurrentUser)
-	fmt.Println(cfg.DbUrl)
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("not enough arguments.")
+		os.Exit(1)
+	}
+
+	fmt.Println(args)
+
+	cmd := command{
+		name: args[1],
+		args: args[2:],
+	}
+
+	err = cmds.run(&ste, cmd)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return errors.New("username required")
+	}
+
+	err := s.config.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User set to %s\n", cmd.args[0])
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.cmds[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	_, ok := c.cmds[cmd.name]
+
+	if !ok {
+		return errors.New("no such command")
+	}
+
+	err := c.cmds[cmd.name](s, cmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
